90d88c5c5d96f0f6915d27b715427237ab93a3cb
lukas <lukas.weber.4.12@gmail.com>
2022-04-25 22:21:14 +0200

    use pathlib.Path

--- CopyFilesFromSubFolders.py
+++ CopyFilesFromSubFolders.py
@@ -7,7 +7,7 @@ from typing import Optional
 import pandas
 
 
-def process_csv_files_from_source(source: str, target: str) -> None:
+def process_csv_files_from_source(source: pathlib.Path, target: pathlib.Path) -> None:
     source_files = find_csv_files(source)
     if source_files is None:
         return
@@ -15,36 +15,36 @@ def process_csv_files_from_source(source: str, target: str) -> None:
     save_combined_files(source_files, target)
 
 
-def find_csv_files(path_name: str) -> Optional[list[str]]:
-    if not os.path.isdir(path_name):
-        print(f'>> error: folder not found: {path_name}')
+def find_csv_files(root_path: pathlib.Path) -> Optional[list[str]]:
+    if not os.path.isdir(root_path):
+        print(f'>> error: folder not found: {root_path}')
         return
-    source_glob = os.path.join(path_name, '**/*time_record*.csv')
+    source_glob = os.path.join(root_path, '**/*time_record*.csv')
     return glob.glob(source_glob, recursive=True)
 
 
-def copy_files_to_target(source_files: list[str], target: str) -> None:
+def copy_files_to_target(source_files: list[str], target: pathlib.Path) -> None:
     create_folder_if_not_exists(target)
     for index, source_file in enumerate(source_files):
         copy_file_to_target(index, source_file, target)
     print(f">> files copied: {len(source_files)}")
 
 
-def create_folder_if_not_exists(path_name: str) -> None:
+def create_folder_if_not_exists(path_name: pathlib.Path) -> None:
     os.makedirs(path_name, exist_ok=True)
 
 
-def copy_file_to_target(index: int, source_file: str, target: str) -> None:
+def copy_file_to_target(index: int, source_file: str, target: pathlib.Path) -> None:
     print(f'>> copy {source_file}')
     target_file = get_target_file_with_index(source_file, target, index)
     shutil.copy(source_file, target_file)
 
 
-def get_target_file_with_index(source_file: str, target: str, index: int) -> pathlib.Path:
+def get_target_file_with_index(source_file: str, target: pathlib.Path, index: int) -> pathlib.Path:
     return get_target_file(source_file, target).with_suffix(f'.{index + 1}.csv')
 
 
-def save_combined_files(source_files: list[str], target) -> None:
+def save_combined_files(source_files: list[str], target: pathlib.Path) -> None:
     if len(source_files) == 0:
         return
     target_file = get_target_file(source_file=source_files[0], target=target)
@@ -60,7 +60,7 @@ def save_excel_file(csv_file: pathlib.Path) -> None:
     print(f'>> combine files in: {excel_file}')
 
 
-def get_target_file(source_file: str, target: str) -> pathlib.Path:
+def get_target_file(source_file: str, target: pathlib.Path) -> pathlib.Path:
     source_filename = os.path.basename(source_file)
     return pathlib.Path(target, source_filename)
 
@@ -79,7 +79,7 @@ def get_csv_file_header_and_content(source_file: str) -> tuple[str, list[str]]:
 
 
 if __name__ == '__main__':
-    source_folder = "quellordner"
-    target_folder = "zielordner"
+    source_folder = pathlib.Path("quellordner")
+    target_folder = pathlib.Path("zielordner")
 
     process_csv_files_from_source(source_folder, target_folder)
