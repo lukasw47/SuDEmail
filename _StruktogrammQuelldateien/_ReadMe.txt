78d0ea449b583bd789fc5f31f6b219a2d9084856
lukas <lukas.weber.4.12@gmail.com>
2022-04-25 15:51:13 +0200

    Initial commit

---
+++ _StruktogrammQuelldateien/_ReadMe.txt
@@ -0,0 +1,7 @@
+Diese Dateien können mit dem Struktogrammer geöffnet werden:
+
+http://www.struktogrammer.ch/Web_files/page1_JavaVersion.html
+
+
+Beste Grüße, 
+Ivanov
---
+++ copyFilesFromSubfolders.py
@@ -0,0 +1,49 @@
+# copyFilesFromSubfolders
+# Author:   Ivanov
+# Revision: 0.2
+#
+
+import os
+import shutil
+import glob
+
+
+def scanFolder(source, target):
+    """ Scans a sourcefolder for csv-files and copies them to target.
+        Returns number of files copied."""
+
+    numberOfFilesCopied = 0
+
+    for content in os.scandir(source):
+        if content.is_dir():
+            print(content.path)
+            numberOfFilesCopied += scanFolder(content.path, target)
+        elif content.is_file():
+            if content.path.endswith(".csv"):
+                print(content.path)
+                shutil.copy(content, target)
+                numberOfFilesCopied += 1
+
+    return numberOfFilesCopied
+
+
+def scan_folder(source, target):
+    source_glob = os.path.join(source, '**/*.csv')
+    source_files = glob.glob(source_glob, recursive=True)
+    for source_file in source_files:
+        print(f'>> copy ')
+        source_filename = os.path.basename(source_file)
+        target_file = os.path.join(target, source_filename)
+        shutil.copy(source_file, target_file)
+    return len(source_files)
+
+
+if __name__ == '__main__':
+    # configuration
+    path = "."
+    sourceFolderName = os.path.join(path, "quellordner")
+    targetFolderName = os.path.join(path, "zielordner")
+
+    # action
+    resultNumber = scanFolder(sourceFolderName, targetFolderName)
+    print(f"Anzahl kopierter Dateien:{resultNumber}")
