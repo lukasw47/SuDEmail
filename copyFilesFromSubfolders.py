2c5b1d0e0e0704a469331858fdf17c7ca1e64f96
lukas <lukas.weber.4.12@gmail.com>
2022-04-25 18:54:50 +0200

    rearrange code

--- copyFilesFromSubfolders.py
+++ copyFilesFromSubfolders.py
@@ -12,10 +12,6 @@ def scan_folder(source: str, target: str) -> None:
     print(f'>> combine\n{combine_csv_files(files=source_files)}')
 
 
-def create_folder_if_not_exists(path_name: str) -> None:
-    os.makedirs(path_name, exist_ok=True)
-
-
 def find_csv_files(path_name: str) -> Optional[list[str]]:
     if not os.path.isdir(path_name):
         print(f'>> error: folder not found: {path_name}')
@@ -32,6 +28,10 @@ def copy_files_to_target(files: list[str], target: str) -> None:
         target_file = os.path.join(target, source_filename)
         shutil.copy(source_file, target_file)
     print(f">> files copied: {len(files)}")
+
+
+def create_folder_if_not_exists(path_name: str) -> None:
+    os.makedirs(path_name, exist_ok=True)
 
 
 def combine_csv_files(files: list[str]) -> str:
